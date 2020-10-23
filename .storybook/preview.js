8ec96c337657cc5f8786f4c0619d332480676dcc
Robert Łukasik <rolukasik@gmail.com>
2020-10-23 20:32:02 +0200

    feat(storybook): add themes provider to storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,13 @@
+import { addDecorator } from '@storybook/react';
+import { withThemesProvider } from 'storybook-addon-styled-component-theme';
+
+import darkMode from '../src/styles/theme/darkMode.ts';
+import lightMode from '../src/styles/theme/lightMode.ts';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
+
+const themes = [darkMode, lightMode];
+
+addDecorator(withThemesProvider(themes));
