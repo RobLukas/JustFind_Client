d17c9c2eeb15d2a67ce058e7d634a42824e1fca1
Robert Łukasik <rolukasik@gmail.com>
2020-10-30 15:57:20 +0100

    feat(offers): add more random variables for offers data

--- src/api/offers.ts
+++ src/api/offers.ts
@@ -1,19 +1,29 @@
 import Offers from './offers.interface';
+import { techCollection } from './technologyCollection';
 
 export default [...Array(30)].map(
   (): Offers => ({
-    mainlyTechnology: 'JS',
+    mainlyTechnology:
+      techCollection[Math.floor(Math.random() * techCollection.length)],
     logoSrc:
       'https://bucket.justjoin.it/offers/company_logos/thumb/b4a0837f135fe43ab93209dbd8e4ce660cbd39b0.jpg?1576668518',
     title: 'Frontend Developer React.js',
     salary: {
-      from: 1000,
-      to: 5000,
+      from:
+        Math.round((Math.floor(Math.random() * (5000 - 1000)) + 1000) / 1000) *
+        1000,
+      to:
+        Math.round((Math.floor(Math.random() * (20000 - 5000)) + 5000) / 1000) *
+        1000,
       currency: 'PLN',
     },
     companyName: 'Sii',
     companyAddress: 'Grunwaldzka 20',
     technologies: ['css', 'react', 'html'],
+    geoPosition: [
+      Number((Math.random() * (54.79086 - 49.29899) + 49.29899).toFixed(5)),
+      Number((Math.random() * (23.89251 - 14.24712) + 14.24712).toFixed(5)),
+    ],
     createdAt: new Date(
       new Date().setDate(new Date().getDate() - Math.floor(Math.random() * 11)),
     ),
--- src/api/technologyCollection.ts
+++ src/api/technologyCollection.ts
@@ -11,4 +11,18 @@ type TechnologyType =
   | '.Net'
   | 'C++';
 
+export const techCollection: TechnologyType[] = [
+  'JS',
+  'Java',
+  'Data',
+  'Blockchain',
+  'Html',
+  'Mobile',
+  'PHP',
+  'Python',
+  'Testing',
+  '.Net',
+  'C++',
+];
+
 export default TechnologyType;
